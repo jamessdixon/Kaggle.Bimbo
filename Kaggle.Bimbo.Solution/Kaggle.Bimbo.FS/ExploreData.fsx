1f858b5fb835ab3ac6c655ed61fabf0dd4eca900
Jamie Dixon <jamessdixon@gmail.com>
2016-06-16 09:10:39 -0400

    added prep data

--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/ExploreData.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/ExploreData.fsx
@@ -3,74 +3,69 @@
 
 open FSharp.Data
 
-type TownState = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/town_state.csv">
+type Town_State = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/town_state.csv">
 type ClienteTabla = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/cliente_tabla.csv">
 type ProductoTabla = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/producto_tabla.csv">
 type Train = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/train.csv",InferRows=100>
 type Test = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/test.csv",InferRows=100>
 
-let townState = TownState.GetSample().Rows
-let clienteTabla = ClienteTabla.GetSample().Rows
-let productoTabla = ProductoTabla.GetSample().Rows
-let train = Train.GetSample().Rows |> Seq.take(100000)
-let test = Test.GetSample().Rows
+let town_States = Town_State.GetSample().Rows
+let clientes = ClienteTabla.GetSample().Rows
+let productos = ProductoTabla.GetSample().Rows
+let trainItems = Train.GetSample().Rows |> Seq.take(100000)
+let testItems = Test.GetSample().Rows
 
 //parse townState
 //townState 5,21,37
-townState |> Seq.take(3)
+town_States |> Seq.take(3)
 //1110, "2008 AG. LAGO FILT", "MÉXICO, D.F."
 //1111, "2002 AG. AZCAPOTZALCO", "MÉXICO, D.F."
-let firstTownState = townState |> Seq.head
+let firstTownState = town_States |> Seq.head
+//firstTownState.Agencia_ID
+//firstTownState.State
+//firstTownState.Town
+
 let states = 
-    townState 
+    town_States 
     |> Seq.groupBy(fun r -> r.State)
     |> Seq.map(fun (f,s) -> f, s|> Seq.length)
     |> Seq.sortByDescending(fun (f,s) -> s)
     |> Seq.iter(fun (f,s) -> printfn "%s %i" f s)
 
 let towns = 
-    townState 
+    town_States 
     |> Seq.groupBy(fun r -> r.Town)
     |> Seq.map(fun (f,s) -> f, s|> Seq.length)
     |> Seq.sortByDescending(fun (f,s) -> s)
     |> Seq.iter(fun (f,s) -> printfn "%s %i" f s)
 
-//firstTownState.Agencia_ID -- Sales Depot ID
-//firstTownState.State
-//firstTownState.Town
-
-//parse the townid from the description
-
-let testRegEx = "2015 AG. ROJO GOMEZ"
-testRegEx.Substring(0,4)
-testRegEx.Substring(5,testRegEx.Length-5)
-
+town_States |> Seq.filter(fun r -> r.Town = "2013 AG. MEGA NAUCALPAN")
 
 //parse clienteTabla 
 //clienteTabla 10,72
-clienteTabla |> Seq.take(3)
+clientes |> Seq.take(3)
 //0, "SIN NOMBRE"
 //1, "OXXO XINANTECATL"
 //2, "SIN NOMBRE"
-let firstClient = clienteTabla |> Seq.head
+let firstClient = clientes |> Seq.head
 //firstClient.Cliente_ID
 //firstClient.NombreCliente
 
 //parse productoTabla
-productoTabla |> Seq.take(3)
+productos |> Seq.take(3)
 //0, "NO IDENTIFICADO 0
 //9, "Capuccino Moka 750g NES 9
 //41, "Bimbollos Ext sAjonjoli 6p 480g BIM 41
-let firstProduct = productoTabla |> Seq.head
+let firstProduct = productos |> Seq.head
 //foo.NombreProducto
 //foo.Producto_ID
 
-train |> Seq.take(3)
+trainItems |> Seq.take(3)
 //train 4,2,7,4,7,4,5,4,1,7,4
 //3, 1110, 7, 3301, 15766, 1212, 3, 25.14M, 0, 0.0M, 3
 //3, 1110, 7, 3301, 15766, 1216, 4, 33.52M, 0, 0.0M, 4
 //3, 1110, 7, 3301, 15766, 1238, 4, 39.32M, 0, 0.0M, 4
-let firstTrain = train |> Seq.head
+let firstTrain = trainItems |> Seq.head
 //firstTrain.Agencia_ID
 //firstTrain.Canal_ID
 //firstTrain.Cliente_ID
@@ -80,6 +75,9 @@ let firstTrain = train |> Seq.head
 //firstTrain.Producto_ID
 //firstTrain.Ruta_SAK
 //firstTrain.Semana
+//firstTrain.Venta_hoy
+//firstTrain.Venta_uni_hoy
+
 
 //Train & Test
 //•Semana — Week number (From Thursday to Wednesday)
---
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/PrepareData.fsx
@@ -0,0 +1,73 @@
+﻿#r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+
+open FSharp.Data
+
+type Town_State = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/town_state.csv">
+type ProductoTabla = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/producto_tabla.csv">
+type ClienteTabla = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/cliente_tabla.csv">
+type Train = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/train.csv",InferRows=100>
+type Test = CsvProvider<"C:/Git/Kaggle.Bimbo/Data/test.csv",InferRows=100>
+
+let town_States = Town_State.GetSample().Rows
+let productos = ProductoTabla.GetSample().Rows
+let clientes = ClienteTabla.GetSample().Rows
+let trainItems = Train.GetSample().Rows |> Seq.take(100000)
+let testItems = Test.GetSample().Rows
+
+type TownState = {SalesDepotId:int; TownId:int; TownDesc: string; StateDesc:string}
+
+let TownStates = 
+    town_States 
+    |> Seq.map(fun i -> {SalesDepotId=i.Agencia_ID; TownId=System.Int32.Parse(i.Town.Substring(0,4)); TownDesc=i.Town.Substring(5,i.Town.Length-5); StateDesc=i.State})
+    |> Set.ofSeq
+
+type Product = {ProductId:int; Brand:string; ShortName:string; Weight:option<float>; Quantity: option<float>}
+
+open System.Text.RegularExpressions
+let createProduct (producto_ID, producto) =
+    let shortNameRegEx = new Regex("^(\D*)")
+    let shortName = shortNameRegEx.Match(producto).Value
+    let brandRegEx = new Regex("^.+\s(\D+) \d+$")
+    let brand = brandRegEx.Match(producto)
+    let brand' =
+        match brand.Groups.Count with
+        | 2 -> brand.Groups.[1].Value
+        | _ -> ""
+    let weightRegEx = new Regex("(\d+)(Kg|g)")
+    let weight = weightRegEx.Match(producto)
+    let weight' = 
+        match weight.Groups.Count with
+        | 3 -> let amount = (float)(weight.Groups.[1]).Value
+               let unit = weight.Groups.[2].Value
+               if unit = "Kg" then Some (amount * 100.0) else Some amount 
+        | _ -> None
+    let quantityRegEx = new Regex("(\d+)p ")
+    let quantity = quantityRegEx.Match(producto)
+    let quantity' = 
+        match quantity.Groups.Count with
+        | 2 -> Some ((float)(quantity.Groups.[1]).Value)
+        | _ -> None
+    {ProductId = producto_ID; Brand = brand'; ShortName = shortName; Weight = weight'; Quantity = quantity'}
+
+let products =
+    productos
+    |> Seq.map(fun p -> createProduct(p.Producto_ID, p.NombreProducto))
+    |> Set.ofSeq
+
+type Client = {ClientId:int; ClientDesc:string}
+
+let clients = 
+    clientes 
+    |> Seq.map(fun r -> {ClientId = r.Cliente_ID; ClientDesc = r.NombreCliente})
+    |> Set.ofSeq
+
+type Transaction = {WeekNumber:int; SalesDepotId:int; SalesChannelId: int; SalesRouteId: int; ClientId: int; ProductId: int; 
+                    SalesThisWeek:float; ReturnsNextWeek:float; SalesUnitThisWeek:int; ReturnsUnitNextWeek:int; AdjustedDemand:int}
+
+let trainTransactions =
+    trainItems
+    |> Seq.take(5000)
+    |> Seq.map(fun i -> {WeekNumber=i.Semana; SalesDepotId = i.Agencia_ID; SalesChannelId = i.Canal_ID; SalesRouteId = i.Ruta_SAK; ClientId = i.Cliente_ID;
+                         ProductId = i.Producto_ID; SalesThisWeek = (float)i.Venta_hoy; ReturnsNextWeek = (float)i.Dev_proxima; SalesUnitThisWeek = i.Venta_uni_hoy;
+                         ReturnsUnitNextWeek = i.Dev_uni_proxima; AdjustedDemand = i.Demanda_uni_equil})
+    |> Set.ofSeq
