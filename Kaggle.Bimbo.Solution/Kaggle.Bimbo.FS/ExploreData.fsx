83e78dc5ae0a485e3673af5a702e5795ea25860e
Jamie Dixon <jamessdixon@gmail.com>
2016-06-23 21:25:17 -0400

    Did some basic exploration

--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/ExploreData.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/ExploreData.fsx
@@ -6,15 +6,67 @@ let all = PrepareData.All
 let townState = PrepareData.getTownStates all
 let products = PrepareData.getProducts all
 let clients = PrepareData.getClients all
-let trainItems = PrepareData.getTrainItems all
-let testItems = PrepareData.getTestItems all
-
-//Real: 00:06:23.808, CPU: 00:05:53.156, GC gen0: 10991, gen1: 2940, gen2: 38
-//Real: 00:01:42.469, CPU: 00:01:38.843, GC gen0: 786, gen1: 226, gen2: 10
+let trainItems = PrepareData.getTrainItems (PrepareData.Random 0.05)
+let testItems = PrepareData.getTestItems (PrepareData.Random 0.05)
 
 trainItems 
-|> Seq.groupBy(fun ti -> ti.WeekNumber)
-|> Seq.map(fun (f,s) -> f, s |> Seq.length)
+|> Seq.distinctBy(fun ti -> ti.WeekNumber)
+|> Seq.map(fun ti -> ti.WeekNumber)
+|> Seq.iter(fun w -> printfn "%i" w)
+// 3,4,5,6,7,8,9
+
+#r "../packages/FSharp.Charting/lib/net40/FSharp.Charting.dll"
+open FSharp.Charting
+
+let adjustedDemand =
+    trainItems
+    |> Seq.map(fun ti -> ti.AdjustedDemand)
+    |> Seq.map(fun ad -> float ad)
+    |> Seq.toArray
+
+adjustedDemand
+|> Chart.FastPoint
+
+adjustedDemand
+|> Array.average
+//7.234130686
+
+let sampleCount =
+    adjustedDemand
+    |> Array.length
+    //3709817
+
+let midpoint = sampleCount / 2
+//1854908
+
+adjustedDemand
+|> Array.sort
+|> Array.skip(midpoint)
+|> Array.head
+//3.0
+
+let maxValue =
+    adjustedDemand
+    |> Array.sortDescending
+    |> Array.head
+// 3960
+
+//mean is 7.24, median is 3.0 -> skewed left
+//min is 0, max is 3,960
+
+let adjustedDemandGrouping =
+    trainItems
+    |> Seq.groupBy(fun ti -> ti.AdjustedDemand)
+    |> Seq.map(fun (f,s) -> f, s |> Seq.length)
+    |> Seq.sortBy(fun (f,s) -> f)
+    |> Seq.toArray
+
+adjustedDemandGrouping
+|> Chart.FastLine
+
+
+
+
 
 
 
--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/PrepareData.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/PrepareData.fsx
@@ -1,5 +1,5 @@
 ﻿
-let basePath = @"C:\Git\Kaggle.Bimbo\Data\"
+let basePath = @"F:\Git\Kaggle.Bimbo\Data\"
 
 type TownState = {SalesDepotId:int; TownId:int; TownDesc:string; StateDesc:string}
 type Product = {ProductId:int; Brand:string; ShortName:string; Weight:option<float>; Quantity: option<float>}
