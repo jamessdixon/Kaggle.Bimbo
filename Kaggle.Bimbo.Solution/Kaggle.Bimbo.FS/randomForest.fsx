7da9c53322913547d8a601f789ab5ddc4fe45a18
Jamie Dixon <jamessdixon@gmail.com>
2016-07-04 16:43:30 -0400

    added validation

--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/randomForest.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/randomForest.fsx
@@ -5,21 +5,34 @@
 #time
 let trainItems = 
     PrepareData.getTrainItems (PrepareData.Random 0.02)
-
+    
 // NOTE: ALGLIB wants prediction variable at end of input array
 let xy = 
     trainItems 
-    |> Seq.map(fun ti -> [|ti.WeekNumber; ti.SalesDepotId; ti.AdjustedDemand|] |> Array.map float)   
+    |> Seq.map(fun ti -> [|ti.WeekNumber; ti.SalesDepotId; ti.ClientId; ti.ProductId; ti.SalesRouteId; ti.AdjustedDemand|] |> Array.map float)   
     |> array2D
     
 let numberOfPoints = trainItems |> Seq.length
-let numberOfVariables = 2
+let numberOfVariables = 5
 let numberOfClasses = 1;
 let numberOfTrees = 50
 let r = 0.1
+let mutable info = 0
+let forest = alglib.dforest.decisionforest()
+let report = alglib.dforest.dfreport()
 
-let info, forest, report = 
-    alglib.dfbuildrandomdecisionforest(xy, numberOfPoints, numberOfVariables, numberOfClasses,numberOfTrees,r)
+alglib.dforest.dfbuildrandomdecisionforest(xy, numberOfPoints, numberOfVariables, numberOfClasses,numberOfTrees,r,&info,forest,report)
 
-report
+let testItems =
+    PrepareData.getTrainItems (PrepareData.Random 0.01)
+
+let makePrediction (item:PrepareData.TrainItem) =
+    let x = item |> fun ti -> [|ti.WeekNumber; ti.SalesDepotId; ti.ClientId; ti.ProductId; ti.SalesRouteId |] |> Array.map float
+    let mutable predictions : float[] = [||]
+    alglib.dforest.dfprocess(forest,x,&predictions)
+    predictions.[0]
+
+testItems
+|> Seq.map(fun ti -> float ti.AdjustedDemand, makePrediction ti)
+|> Seq.averageBy(fun (a,p) -> a-p)
 
