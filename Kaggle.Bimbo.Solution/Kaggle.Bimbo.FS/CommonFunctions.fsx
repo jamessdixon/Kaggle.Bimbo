f06cef8e2113f2642fb691920e7d0697ec9b0cc5
Jamie Dixon <jamessdixon@gmail.com>
2016-07-04 21:06:25 -0400

    run some random forests

--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/CommonFunctions.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/CommonFunctions.fsx
@@ -3,11 +3,17 @@ open System
 
 type ExperimentResult = {Simulated:float; Observed:int}
 
+let RMSE (results:ExperimentResult[])=
+    results
+    |> Seq.averageBy (fun r ->
+        let delta = r.Simulated - ((float)r.Observed)
+        delta * delta)
+    |> sqrt
+
+
 let RMSLE (results:ExperimentResult[]) =
     results
     |> Seq.averageBy(fun r -> Math.Pow((log((r.Simulated + 1.0)) - log(float (r.Observed + 1))), 2.0))
     |> sqrt
 
-//let test = [|{Simulated = 18.66; Observed = 0;}|]
-//RMSLE test
 
--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/randomForest.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/randomForest.fsx
@@ -8,25 +8,26 @@
 let trainItems = 
     PrepareData.getTrainItems (PrepareData.Random 0.02)
     
+let testItems =
+    PrepareData.getTrainItems (PrepareData.Random 0.01)
+
 // NOTE: ALGLIB wants prediction variable at end of input array
 let xy = 
     trainItems 
     |> Seq.map(fun ti -> [|ti.WeekNumber; ti.SalesDepotId; ti.ClientId; ti.ProductId; ti.SalesRouteId; ti.AdjustedDemand|] |> Array.map float)   
     |> array2D
     
+
 let numberOfPoints = trainItems |> Seq.length
 let numberOfVariables = 5
 let numberOfClasses = 1;
-let numberOfTrees = 50
+let numberOfTrees = 150
 let r = 0.1
 let mutable info = 0
 let forest = alglib.dforest.decisionforest()
 let report = alglib.dforest.dfreport()
 
 alglib.dforest.dfbuildrandomdecisionforest(xy, numberOfPoints, numberOfVariables, numberOfClasses,numberOfTrees,r,&info,forest,report)
-
-let testItems =
-    PrepareData.getTrainItems (PrepareData.Random 0.01)
 
 let makePrediction (item:PrepareData.TrainItem) =
     let x = item |> fun ti -> [|ti.WeekNumber; ti.SalesDepotId; ti.ClientId; ti.ProductId; ti.SalesRouteId |] |> Array.map float
@@ -36,9 +37,16 @@ let makePrediction (item:PrepareData.TrainItem) =
 
 open CommonFunctions
 
-testItems
-|> Seq.map(fun ti -> {Simulated=makePrediction ti; Observed=ti.AdjustedDemand})
-|> Seq.toArray
-|> RMSLE
+let rmsle = 
+    testItems
+    |> Seq.map(fun ti -> {Simulated=makePrediction ti; Observed=ti.AdjustedDemand})
+    |> Seq.toArray
+    |> RMSLE
+
+printfn "rmsError %A, oobError %A rmsle %A" report.rmserror report.oobrmserror rmsle
+//2% train/1% test/050 trees/r=.1 = rmsError 16.90219728, oobError 18.82415846 rmsle 0.7196989171   03.42 to train model
+//2% train/1% test/100 trees/r=.1 = rmsError 16.88868484, oobError 18.68523038 rmsle 0.7195092518   09:17 to train model
+//2% train/1% test/150 trees/r=.1 = rmsError 16.79528265, oobError 18.64584115 rmsle 0.7160595963   14:22 to train model
+
 
 
