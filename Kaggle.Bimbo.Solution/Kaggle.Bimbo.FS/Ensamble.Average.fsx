1596c476873aa692f99085dfc06b69f0f79af7e6
Jamie Dixon <jamessdixon@gmail.com>
2016-07-15 09:36:12 -0400

    added analysis of errors

--- Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/Ensamble.Average.fsx
+++ Kaggle.Bimbo.Solution/Kaggle.Bimbo.FS/Ensamble.Average.fsx
@@ -33,11 +33,39 @@ let ensambles =
              Common.LimitedEnsamble.GLM=glm.[i]})
     |> Seq.toArray
 
-let blendedRMSLE =
-    ensambles
-    |> Seq.map(fun e -> {Common.ExperimentResult.Observed = e.Observed; Common.ExperimentResult.Simulated= ((e.RandomForest + e.GLM)/2.0)})
-    |> Seq.toArray
-    |> Common.RMSLE
+let totalCount = ensambles |> Seq.length //742114
 
+let foo = 
+    ensambles
+    |> Seq.groupBy(fun e -> abs((int e.RandomForest) - e.Observed))
+    |> Seq.map(fun (v,a) -> v, a |> Seq.length)
+    |> Seq.sortBy(fun (k,v) -> k)
+    |> Seq.take(20)
+
+let bar = 
+    ensambles
+    |> Seq.groupBy(fun e -> abs((int e.GLM) - e.Observed))
+    |> Seq.map(fun (v,a) -> v, a |> Seq.length)
+    |> Seq.sortBy(fun (k,v) -> k)
+    |> Seq.take(20)
+
+Seq.zip foo bar
+|> Seq.iter(fun (f,s) -> printfn "%A , %A , %A" (fst f) (snd f) (snd s))
+
+Seq.zip foo bar
+|> Seq.iter(fun (f,s) -> printfn "%A" (fst f))
+
+Seq.zip foo bar
+|> Seq.iter(fun (f,s) -> printfn "%A" (snd f))
+
+Seq.zip foo bar
+|> Seq.iter(fun (f,s) -> printfn "%A" (snd s))
+
+//let blendedRMSLE =
+//    ensambles
+//    |> Seq.map(fun e -> {Common.ExperimentResult.Observed = e.Observed; Common.ExperimentResult.Simulated= ((e.RandomForest + e.GLM)/2.0)})
+//    |> Seq.toArray
+//    |> Common.RMSLE
+//
 //RF (.66) and GLM (.82) = .67
 
